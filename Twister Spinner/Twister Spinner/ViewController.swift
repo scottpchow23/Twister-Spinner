8ca2e4f402d2254f2d6ef3d8325699af56eae356
Scott Chow <scottpchow@umail.ucsb.edu>
2017-06-23 16:59:44 -0700

    Siri now calls out the move.

--- Twister Spinner/Twister Spinner/ViewController.swift
+++ Twister Spinner/Twister Spinner/ViewController.swift
@@ -7,11 +7,16 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
     let bodyParts : [String] = ["left hand", "right hand", "left foot", "right foot"]
-    let colors : [UIColor] = [UIColor.red, UIColor.blue, UIColor.green, UIColor.yellow];
+    let colors : [UIColor] = [UIColor.red, UIColor.blue, UIColor.green, UIColor.yellow]
+    let colorNames : [String] = ["red", "blue", "green", "yellow"]
+    let bodyImages : [UIImage] = [#imageLiteral(resourceName: "icons8-Left Hand"), #imageLiteral(resourceName: "icons8-Right Hand"), #imageLiteral(resourceName: "icons8-Left Footprint"), #imageLiteral(resourceName: "icons8-Right Footprint")]
+    let synth = AVSpeechSynthesizer()
     
+    @IBOutlet weak var bodyPartImageView: UIImageView!
     @IBOutlet weak var bodyPartLabel: UILabel!
     
     override func viewDidLoad() {
@@ -25,10 +30,15 @@ class ViewController: UIViewController {
     }
     
     func spin() {
-        let spin = (bodyParts[Int(arc4random()%4)], colors[Int(arc4random()%4)])
+        let bodySpin = Int(arc4random()%4)
+        let colorSpin = Int(arc4random()%4)
+        let spin = (bodyParts[bodySpin], colors[colorSpin], bodyImages[bodySpin], colorNames[colorSpin])
         print(spin)
+        self.bodyPartImageView.image = spin.2
         self.view.backgroundColor = spin.1
         self.bodyPartLabel.text = spin.0
+        let utterance = AVSpeechUtterance(string: "\(spin.0) to \(spin.3) ")
+        synth.speak(utterance)
     }
 
 
